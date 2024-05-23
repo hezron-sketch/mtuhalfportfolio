38a8e16cc37e915fcf6542593261b1655a1af002
mtu-half <hpaulhezne@gmail.com>
2024-05-23 11:54:03 +0300

    a short intro

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,9 @@ export default function Home() {
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 "></h2>
+        <h2 className="text-sm text-zinc-500 ">
+          A computer enthusiast writing code for money
+        </h2>
       </div>
     </div>
   );
