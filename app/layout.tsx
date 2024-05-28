e918ca966800c3f14ac75ec6c2c734c822fbf670
mtu-half <hpaulhezne@gmail.com>
2024-05-28 15:10:10 +0300

    Removed: project default descriptions

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,18 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "mtuhalfportfolio",
+    template: "mtuhalf",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "This portfolio is developed in nextjs",
   openGraph: {
-    title: "chronark.com",
-    description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    title: "mtuhalfportfolio",
+    description: "mtuhalfportfolio in Nextjs",
+    url: "https://github.com/hezron-sketch/mtuhalfportfolio",
+    siteName: "mtuhalf",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "",
         width: 1920,
         height: 1080,
       },
@@ -66,8 +65,9 @@ export default function RootLayout({
         <Analytics />
       </head>
       <body
-        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
-          }`}
+        className={`bg-black ${
+          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+        }`}
       >
         {children}
       </body>
